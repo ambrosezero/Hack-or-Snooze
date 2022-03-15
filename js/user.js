03b4e70204016d7dd6b25ae1ae4fda990e738160
Lance Shaw <AmbroseSouth@outlook.com>
2022-03-15 11:07:27 -0500

    fixed 'sign-up with existing username' error

--- js/user.js
+++ js/user.js
@@ -43,10 +43,17 @@ async function signup(evt) {
 
   // User.signup retrieves user info from API and returns User instance
   // which we'll make the globally-available, logged-in user.
-  currentUser = await User.signup(username, password, name);
+  try {
+    currentUser = await User.signup(username, password, name);
+    saveUserCredentialsInLocalStorage();
+    updateUIOnUserLogin();
+  } catch {
+    alert('that username already exists');
+  }
 
-  saveUserCredentialsInLocalStorage();
-  updateUIOnUserLogin();
+
+  // saveUserCredentialsInLocalStorage();
+  // updateUIOnUserLogin();
 
   $signupForm.trigger("reset");
   hidePageComponents();
